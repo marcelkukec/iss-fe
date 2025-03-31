027e1d3d7129d9f64237b26798bc988e2638890f
Marcel Kukec <marcel.kukec.mk@gmail.com>
2025-03-31 19:29:49 +0200

    User join/leave group

--- src/components/Group.tsx
+++ src/components/Group.tsx
@@ -2,6 +2,7 @@ import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import api from "../api/api";
 import PostCard from "../components/PostCard";
+import {useAuth} from "../context/AuthContext.tsx";
 
 interface PostData {
     id: number;
@@ -13,9 +14,12 @@ interface PostData {
 
 export default function Group() {
     const { id } = useParams();
+    const { isLoggedIn } = useAuth();
     const [posts, setPosts] = useState<PostData[]>([]);
     const [groupName, setGroupName] = useState('');
     const [groupDesc, setGroupDesc] = useState('');
+    const [isMember, setIsMember] = useState(false);
+    const [refresh, setRefresh] = useState(false);
 
     useEffect(() => {
         api.get(`/groups/${id}`).then(res => {
@@ -26,7 +30,26 @@ export default function Group() {
         api.get(`/posts/group/${id}`).then(res => {
             setPosts(res.data);
         });
-    }, [id]);
+
+        api.get(`/user-groups/is-member/${id}`).then(res => {
+            setIsMember(res.data.isMember);
+        });
+    }, [id, refresh]);
+
+    const handleToggleMembership = async () => {
+        try {
+            if(isMember) {
+                await api.delete(`/user-groups/leave/${id}`);
+            } else {
+                await api.post(`/user-groups/join/${id}`);
+            }
+            setRefresh(prev => !prev);
+
+        } catch (err) {
+            console.error('Failed to update group membership', err);
+            alert('Something went wrong!');
+        }
+    }
 
     useEffect(() => {
         document.title = groupName;
@@ -34,6 +57,16 @@ export default function Group() {
 
     return (
         <div className="container mt-4">
+
+            {isLoggedIn && (
+                <button
+                    className={`btn ${isMember ? 'btn-outline-danger' : 'btn-outline-success'} mb-4`}
+                    onClick={handleToggleMembership}
+                >
+                    {isMember ? 'Leave Group' : 'Join Group'}
+                </button>
+            )}
+
             <h2 className="mb-2">{groupName}</h2>
             <h6 className="mb-4 text-muted">{groupDesc}</h6>
             {posts.length > 0 ? (
